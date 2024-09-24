ed0341969f53e757cbc05997d0f0095b9cd734b6
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-24 15:53:21 +0700

    ini pengerjaanya

--- Fixerly/Pengerjaan.java
+++ Fixerly/Pengerjaan.java
@@ -1,42 +1,59 @@
-import java.time.LocalDate;
-
 public class Pengerjaan {
-    private LocalDate tanggal;
+    private String tanggal; // Mengganti LocalDate dengan String
     private Tukang tukang;
     private Customer customer;
     private String status;
 
-    public Pengerjaan(LocalDate tanggal, Tukang tukang, Customer customer) {
+    //ini constructor berguna ketika mau buat object baru
+    public Pengerjaan(String tanggal, Tukang tukang, Customer customer) {
         this.tanggal = tanggal;
         this.tukang = tukang;
         this.customer = customer;
         this.status = "Pending";
     }
 
-    public void assignTukang(Tukang tukang) {
-        this.tukang = tukang;
+    // setter ini dipake untuk mengubah tanggal
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
     }
 
-    public void assignCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
+    //ini buat update job status entah itu pending, complete, ataupun gagal
     public void updateJobStatus(String status) {
         this.status = status;
     }
 
-    public LocalDate getTanggal() {
+    //ini buat show details kapan dikerjain siapa si customer dan siapa si tukangnya
+    public void showDetails() {
+        if (tanggal != null) {
+            System.out.println("Pengerjaan dibuat: Tanggal - " + tanggal +
+                    "\nTukang - " + tukang.getIdTukang() +
+                    "\nCustomer - " + customer.getIdCustomer());
+        } else {
+            System.out.println("Tanggal pengerjaan belum diatur.");
+        }
+    }
+
+    //ini buat ngasih tau status pengerjaanya
+    public void showStatus() {
+        System.out.println("Status pengerjaan: " + status);
+    }
+
+    //ini buat getter si tanggalnya
+    public String getTanggal() {
         return tanggal;
     }
 
+    //ini buat ngambil si tukangnya
     public Tukang getTukang() {
         return tukang;
     }
 
+    //ini buat ngambil si customernya
     public Customer getCustomer() {
         return customer;
     }
 
+    //ini buat ambil statusnya
     public String getStatus() {
         return status;
     }
